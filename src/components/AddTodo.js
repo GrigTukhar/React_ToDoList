1dd1d3abc73ef4d0922be8feb8679e13e963b1a8
GrigTukhar <grigor_tukharyan@edu.aua.am>
2019-08-12 14:38:43 +0400

    Adding error validation and getting rid of alert popup

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,43 +1,70 @@
 import React, { Component } from 'react'
 
 
+
 class AddTodo extends Component {
+    
     state ={
-        title: ""
+        title: "",
+        isValid: true
     }
 
     //takes title and makes new todo, clears the bar
     onSubmit = (e)=> {
         e.preventDefault();
-        if (this.state.title.length >10){
-            this.props.addTodo(this.state.title);
+        let isValid = this.state.title.length > 10;
+        if (isValid){
+            this.props.addTodo(this.state.title);       
         }
-        else{
-            window.alert("Please input a todo of more than 10 characters")
-        }
+
+        this.setState({isValid});
         this.setState({title: " "});
     }
 
     //saves title
     onChange=(e)=>this.setState({ [e.target.name]: e.target.value});
 
+
+
     render() {
+        let table, submitButton;
+        if(this.state.isValid ===true){
+            table =<input 
+            type="text" 
+            name="title"
+            
+            style={{flex:"6", padding:"5px"}}
+            placeholder="Add Todo with more than 10 characters ..."
+            value={this.state.title}
+            onChange={this.onChange}
+           />
+           submitButton= <input 
+           type="submit"
+           value="Submit"
+           className="btn"
+           style={{flex: "1"}}
+          />
+        }
+        else{
+            table =<input 
+            type="text" 
+            name="title"
+            style={{flex:"6", padding:"5px", borderColor:'red'}}
+            placeholder="Error ..."
+            value={this.state.title}
+            onChange={this.onChange}
+           />
+           submitButton= <input 
+           type="submit"
+           value="More than 10 characters required"
+           className="btn2"
+           style={{flex: "1"}}
+          />
+        }
         return (
-           <form onSubmit={this.onSubmit} style ={{display: "flex"}}>
-               <input 
-                type="text" 
-                name="title"
-                style={{flex:"10", padding:"5px"}}
-                placeholder="Add Todo with more than 10 characters ..."
-                value={this.state.title}
-                onChange={this.onChange}
-               />
-               <input 
-                type="submit"
-                value="Submit"
-                className="btn"
-                style={{flex: "1"}}
-               />
+           <form onSubmit={this.onSubmit} style ={{display: "flex"}} >
+               {table}
+               {submitButton}
            </form>
         )
     }
